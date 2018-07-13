155a5a770e33d4ee18415482bf0007f26875190f
root <root@kb-ark-ragnarok.c.tim-christoph-lid.internal>
2018-07-13 21:43:35 +0000

    first commit2

--- pre_installations/mcmyadmin.sh
+++ pre_installations/mcmyadmin.sh
@@ -1,8 +1,8 @@
 sudo wget http://mcmyadmin.com/Downloads/etc.zip -P /usr/local/
 sudo unzip -o /usr/local/etc.zip -d /usr/local; sudo rm /usr/local/etc.zip
 currentpath=$(pwd)
-/./home/tim_c_lid/test/pre_installations/create_user.sh minecraft minecraft
-su -c /./home/tim_c_lid/test/pre_installations/mcmyadmin_user.sh - minecraft
+./pre_installations/create_user.sh minecraft minecraft
+su -c ./pre_installations/mcmyadmin_user.sh - minecraft
 #sudo cp pre_installations/mcmyadmin.service /etc/systemd/system/mcmyadmin.service
 #chmod +x /etc/systemd/system/mcmyadmin.service
 #sudo systemctl enable mcmyadmin; sudo systemctl start mcmyadmin
