045cfbaf2f6080122b1a58c33ac863c9e04eb4b6
root <root@kb-ark-ragnarok.c.tim-christoph-lid.internal>
2018-07-17 20:45:02 +0000

    Disk anpassungen

--- pre_installations/disk1.sh
+++ pre_installations/disk1.sh
@@ -1,10 +1,23 @@
-(
-echo o # Create a new empty DOS partition table
-echo n # Add a new partition
-echo p # Primary partition
-echo 1 # Partition number
-echo   # First sector (Accept default: 1)
-echo   # Last sector (Accept default: varies)
-echo w # Write changes
-) | sudo fdisk /dev/sdb
-mkfs.ext4 /dev/sdb1
+#!/bin/bash
+LINE="/dev/sdb1 /media/disk1 ext4 defaults 0 2"
+FILE=/etc/fstab
+
+if ! grep -qF "$LINE" /etc/fstab
+then
+    (
+    echo o # Create a new empty DOS partition table
+    echo n # Add a new partition
+    echo p # Primary partition
+    echo 1 # Partition number
+    echo   # First sector (Accept default: 1)
+    echo   # Last sector (Accept default: varies)
+    echo w # Write changes
+    ) | sudo fdisk /dev/sdb
+    no | mkfs.ext4 /dev/sdb1
+    mkdir /media/disk1
+    mount /dev/sdb1 /media/disk1
+    echo "#poky-disc" >> /etc/fstab
+    echo "/dev/sdb1 /media/disk1 ext4 defaults 0 2" >> /etc/fstab
+else
+    echo "Entry in fstab exists."
+fi
