9310f868026a442310428b53c707180aee473930
root <root@kb-ark-ragnarok.c.tim-christoph-lid.internal>
2018-07-17 18:39:43 +0000

    Disk1

---
+++ pre_installations/disk1.sh
@@ -0,0 +1,9 @@
+(
+echo o # Create a new empty DOS partition table
+echo n # Add a new partition
+echo p # Primary partition
+echo 1 # Partition number
+echo   # First sector (Accept default: 1)
+echo   # Last sector (Accept default: varies)
+echo w # Write changes
+) | sudo fdisk /dev/sdb
