b8a3a708b465ea5e4feffc1db7ad6f447c868760
root <root@kb-ark-ragnarok.c.tim-christoph-lid.internal>
2018-07-18 16:42:44 +0000

    Ordner automatisch erstellen

--- pre_installations/servicebyname.sh
+++ pre_installations/servicebyname.sh
@@ -1,4 +1,4 @@
-pwd
+sudo mkdir /media/disk1/service
 sudo cp services/start.$1.sh /media/disk1/service/start.sh
 sudo chmod +x /media/disk1/service/start.sh
 sudo cp services/stop.$1.sh /media/disk1/service/stop.sh
