eb524399b87bbc3bf257fcabc8e4afa89a654a8c
root <root@kb-ark-ragnarok.c.tim-christoph-lid.internal>
2018-07-20 22:13:52 +0000

    angepasst

--- pre_installations/darkandlight.sh
+++ pre_installations/darkandlight.sh
@@ -1,4 +1,4 @@
-#./pre_installations/servicebyname.sh darkandlight
+./pre_installations/servicebyname.sh darkandlight
 #echo "run steamcmd darkandlight"
 #steamcmd +login anonymous +force_install_dir /media/disk1/steamcmd +app_update 740 +quit
 #steamcmd +login anonymous +force_install_dir /media/disk1/steamcmd +app_update 740 +quit
