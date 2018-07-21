75f1e9a3937990c2e27427ba5da2f43744c534a8
root <root@kb-ark-ragnarok.c.tim-christoph-lid.internal>
2018-07-21 11:32:00 +0000

    Server korrekt stoppen

--- services/start.darkandlight.sh
+++ services/start.darkandlight.sh
@@ -3,4 +3,4 @@ source /media/disk1/service/config.conf
 ./media/disk1/darkandlight/serverpassword.ssh
 ./media/disk1/darkandlight/servername.ssh
 /usr/games/steamcmd +login anonymous +force_install_dir /media/disk1/darkandlight +app_update 630230 validate +quit
-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine ~/.wine/drive_c/darkandlight/DNL/Binaries/Win64/DNLServer.exe  "/Game/DNL/Maps/CapeOfSacredPath/DNL_ALL?Port=7777?QueryPort=27015?SessionName="$servername"?MaxPlayers=70?listen?AllowFlyerCarryPvE=true?RCONEnabled=True?RCONPort=32330?ServerAdminPassword=Password"$serverpassword -servergamelog -useallavailablecores -nosteamclient -game -server -log
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine ~/.wine/drive_c/darkandlight/DNL/Binaries/Win64/DNLServer.exe  "/Game/DNL/Maps/CapeOfSacredPath/DNL_ALL?Port=7777?QueryPort=27015?SessionName="$servername"?MaxPlayers=70?listen?AllowFlyerCarryPvE=true?RCONEnabled=True?RCONPort=32330?ServerAdminPassword="$serverpassword -servergamelog -useallavailablecores -nosteamclient -game -server -log
--- services/stop.darkandlight.sh
+++ services/stop.darkandlight.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo -u minecraft bash -c "/usr/bin/screen -p 0 -S mc-mcma -X eval 'stuff "/quit"\\015'"
+#sudo -u minecraft bash -c "/usr/bin/screen -p 0 -S mc-mcma -X eval 'stuff "/quit"\\015'"
+source /media/disk1/service/config.conf
+/media/disk1/service/.rcon -P$serverpassword -a$(hostname --ip-address) -p32330 Broadcast Hey, server will be shutdown
+/media/disk1/service/.rcon -P$serverpassword -a$(hostname --ip-address) -p32330 SaveWorld
+sleep 2
+/media/disk1/service/.rcon -P$serverpassword -a$(hostname --ip-address) -p32330 DoExit
