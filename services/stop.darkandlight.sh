0a4947e335d7fe9b1f6fd56539594cee19f4d339
root <root@kb-ark-ragnarok.c.tim-christoph-lid.internal>
2018-07-28 10:57:45 +0000

    Kleine anpassungen

--- services/stop.darkandlight.sh
+++ services/stop.darkandlight.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #sudo -u minecraft bash -c "/usr/bin/screen -p 0 -S mc-mcma -X eval 'stuff "/quit"\\015'"
 source /media/disk1/service/config.conf
-/media/disk1/service/.rcon -P$serverpassword -a$(hostname --ip-address) -p32330 Broadcast Hey, server will be shutdown
-/media/disk1/service/.rcon -P$serverpassword -a$(hostname --ip-address) -p32330 SaveWorld
-sleep 2
-/media/disk1/service/.rcon -P$serverpassword -a$(hostname --ip-address) -p32330 DoExit
+/media/disk1/service/./rcon -P$serverpassword -a$(hostname --ip-address) -p32330 SaveWorld
+/media/disk1/service/./rcon -P$serverpassword -a$(hostname --ip-address) -p32330 Broadcast Hey, Server saved! Shutdown! Good Bye
+/media/disk1/service/./rcon -P$serverpassword -a$(hostname --ip-address) -p32330 DoExit
